4c6a04b177ee3d05572255647811100f6ccac934
Zevi-Friend <mirs212@gmail.com>
2025-06-11 22:46:23 -0400

    Update espnow_sender_bool_Version3.ino

--- WiFi buzzer/espnow_sender_bool_Version3/espnow_sender_bool_Version3.ino
+++ WiFi buzzer/espnow_sender_bool_Version3/espnow_sender_bool_Version3.ino
@@ -2,16 +2,25 @@
 #include <esp_now.h>
 
 const int button = 0;
+const int vol = 32;
 
 //Since I am using the boot pin, pressed == HIGH and vice verca.
-bool myState = HIGH;
+bool myState = LOW;
 bool oldState = myState;
+
+typedef struct struct_message {
+  bool myState;
+  int myValue;
+} struct_message;
+
+struct_message dataToSend = {myState, 0};
 
 // Receiver's MAC address as a byte array
 uint8_t receiverAddress[] = {0x8C, 0x4F, 0x00, 0x30, 0x82, 0xFC}; // your receiver MAC
 
 void setup() {
   pinMode(button, INPUT_PULLUP);
+  pinMode(vol, INPUT);
 
   Serial.begin(115200); // Start serial communication for debug output
   delay(1000);
@@ -37,12 +46,12 @@ void setup() {
 }
 
 void loop() {
-  myState = !digitalRead(button);
+  dataToSend.myState = !digitalRead(button);
 
-  if(myState != oldState) {
-    esp_now_send(receiverAddress, (uint8_t*)&myState, sizeof(myState));
-    oldState = myState;
-    Serial.println(myState);
+  if(dataToSend.myState != oldState) {
+    esp_now_send(receiverAddress, (uint8_t*)&dataToSend, sizeof(dataToSend));
+    oldState = dataToSend.myState;
+    Serial.println(dataToSend.myState);
   }
 
   delay(20);
