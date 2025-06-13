28dd9dcddbe0f55a37f6480640746dc2f28e201b
Zevi-Friend <mirs212@gmail.com>
2025-06-12 22:01:02 -0400

    Update ESP_NOW_Receiver_ESP_NOW_Receiver.ino

--- WiFi buzzer/ESP_NOW_Receiver_ESP_NOW_Receiver/ESP_NOW_Receiver_ESP_NOW_Receiver.ino
+++ WiFi buzzer/ESP_NOW_Receiver_ESP_NOW_Receiver/ESP_NOW_Receiver_ESP_NOW_Receiver.ino
@@ -2,19 +2,30 @@
 #include <esp_now.h>
 
 const int buzzer = 25;
-bool lastState = LOW;
+
+const int freq = 2000;     // 2 kHz tone (adjust for your buzzer)
+const int pwmChannel = 0;  // ESP32 has 16 channels: 0-15
+const int resolution = 8;  // 8-bit (0-255)
+
+typedef struct struct_message {
+  bool myState;
+  int myValue;
+} struct_message;
 
 // New callback signature for ESP32 Arduino core v3.x and later
 void OnDataRecv(const esp_now_recv_info_t *recv_info, const uint8_t *data, int len) {
-  bool incomingState;
-  if (len == sizeof(incomingState)) {
-    memcpy(&incomingState, data, sizeof(incomingState));
+  struct_message incomingData;
+  if (len == sizeof(incomingData)) {
+    memcpy(&incomingData, data, sizeof(incomingData));
     Serial.print("Received state: ");
-    Serial.println(incomingState ? "HIGH (pressed)" : "LOW (released)");
-    if (incomingState) {
-      digitalWrite(buzzer, HIGH); // Buzzer ON
+    Serial.println(incomingData.myState ? "HIGH (pressed)" : "LOW (released)");
+
+    int mapped = map(incomingData.myValue, 0, 4095, 0, 255);
+
+    if (incomingData.myState) {
+      ledcWrite(pwmChannel, mapped);
     } else {
-      digitalWrite(buzzer, LOW);  // Buzzer OFF
+      ledcWrite(pwmChannel, 0);  // Buzzer OFF
     }
   } else {
     Serial.print("Received unexpected data length: ");
@@ -33,6 +44,10 @@ void setup() {
     Serial.println("ESP-NOW init failed!");
     while (true) { delay(1); }
   }
+
+  ledcSetup(pwmChannel, freq, resolution);
+  ledcAttachPin(buzzer, pwmChannel);
+
   esp_now_register_recv_cb(OnDataRecv);
 }
 
